e9c315587493a398e5ff6b7b58bbface05c7a896
Steve King, Jr <stevekx86@gmail.com>
2015-10-06 15:01:00 -0700

    Brainstorming some models that should be present.

--- src/models.py
+++ src/models.py
@@ -1,8 +1,7 @@
-from flask_sqlalchemy import Model
 from __init__ import db
 
 
-class User(Model):
+class User(db.Model):
     __tablename__ = "user"
     ROLE_ADMIN = 0
     ROLE_USER = 1
@@ -14,7 +13,7 @@ class User(Model):
     postings = db.relationship("Posting", backref="user")
 
 
-class Posting(Model):
+class Posting(db.Model):
     __tablename__ = "posting"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Unicode(64), index=True)
@@ -23,3 +22,13 @@ class Posting(Model):
     user_id = db.ForeignKey("user.id", index=True)
 
 
+class Bid(db.Model):
+    __tablename__ = "bid"
+
+
+class PrivateMessage(db.Model):
+    __tablename__ = "message"
+
+
+class Transaction(db.Model):
+    __tablename__ = "transaction"
