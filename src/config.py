cd6584c14c89445f869cacf472e67949ca279474
Nathan Fegard <nfegard@asu.edu>
2015-10-20 02:14:57 -0700

    web dev lyfe

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'mysql://arthur:bingbang@artizanz.stevex86.com/db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///webapp.db'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 CSRF_ENABLED = True
 SECRET_KEY = 'change me when deployed'
