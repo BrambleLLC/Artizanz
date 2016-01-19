f9fde0af0c6c3ec683c3a7b4ff3c6be6f7ad39e5
Steve King, Jr <stevekx86@gmail.com>
2016-01-19 01:13:12 -0700

    Squelched warning by disabling SQLALCHEMY_TRACK_CHANGES

--- src/config.py
+++ src/config.py
@@ -4,5 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 #SQLALCHEMY_DATABASE_URI = 'mysql://arthur:bingbang@artizanz.stevex86.com/db'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///webapp.db'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 CSRF_ENABLED = True
 SECRET_KEY = 'change me when deployed'
