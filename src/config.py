09137635b8bced6173fde1ddd674c94d0679a930
Steve King, Jr <stevekx86@gmail.com>
2016-01-19 17:08:11 -0700

    Configure permanent session lifetime.

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 #SQLALCHEMY_DATABASE_URI = 'mysql://arthur:bingbang@artizanz.stevex86.com/db'
@@ -7,3 +8,4 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 CSRF_ENABLED = True
 SECRET_KEY = 'change me when deployed'
+PERMANENT_SESSION_LIFETIME = timedelta(days=120)
