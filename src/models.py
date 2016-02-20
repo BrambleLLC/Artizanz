2e2931d05b928fbb377d288e409b8e14d597db36
Steve King, Jr <stevekx86@gmail.com>
2016-02-20 14:51:40 -0700

    Scheming
    Fleshed out the Posting model
    Added and fleshed out the Bid model
    Added and began working with PrivateMessageThreads
    Made a secondary association table for the many-to-many relationship between User and MessageThread
    Made and began working on PrivateMessages
    Made and began work on Tags and secondary association table

--- src/models.py
+++ src/models.py
@@ -1,5 +1,13 @@
 from __init__ import db
 
+thread_table = db.Table("threads",
+                        db.Column("user_id", db.Integer, db.ForeignKey("user.id")),
+                        db.Column("thread_id", db.Integer, db.ForeignKey("message_thread.id")))
+
+tag_table = db.Table("tags",
+                     db.Column("tag_id", db.Integer, db.ForeignKey("tag.id")),
+                     db.Column("posting_id", db.Integer, db.ForeignKey("posting.id")))
+
 
 class User(db.Model):
     __tablename__ = "user"
@@ -17,7 +25,8 @@ class User(db.Model):
     zipcode = db.Column(db.Unicode(12))
     phone_number = db.Column(db.Unicode(10))
     role = db.Column(db.Integer, default=ROLE_USER)
-    postings = db.relationship("Posting", backref="user")
+    postings = db.relationship("Posting", backref="user", lazy="dynamic", cascade="all, delete-orphan")
+    bids = db.relationship("Bid", backref="user", lazy="dynamic", cascade="all, delete-orphan")
 
 
 class Posting(db.Model):
@@ -26,17 +35,36 @@ class Posting(db.Model):
     title = db.Column(db.Unicode(64), index=True)
     description = db.Column(db.UnicodeText(1600))
     price = db.Column(db.Integer, default=100)
+    start_time = db.Column(db.DateTime)
+    end_time = db.Column(db.DateTime, index=True)
     user_id = db.ForeignKey("user.id", index=True)
+    bids = db.relationship("Bid", backref="posting", lazy="dynamic", cascade="all, delete-orphan")
+    tags = db.relationship("Tag", secondary=tag_table, backref="postings", lazy="dynamic")
 
-"""
+
 class Bid(db.Model):
     __tablename__ = "bid"
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Numeric(precision=20, scale=2))
+    user_id = db.ForeignKey("user.id", index=True)
+    posting_id = db.ForeignKey("posting.id", index=True)
+
+
+class PrivateMessageThread(db.Model):
+    __tablename__ = "message_thread"
+    id = db.Column(db.Integer, primary_key=True)
+    messages = db.relationship("PrivateMessage", backref="thread", lazy="dynamic", cascade="all, delete-orphan")
+    users = db.relationship("User", secondary=thread_table, backref="private_message_threads", lazy="dynamic")
 
 
 class PrivateMessage(db.Model):
     __tablename__ = "message"
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.UnicodeText)
+    sent_datetime = db.Column(db.DateTime)
 
 
-class Transaction(db.Model):
-    __tablename__ = "transaction"
-"""
+class Tag(db.Model):
+    __tablename__ = "tag"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), unique=True)
