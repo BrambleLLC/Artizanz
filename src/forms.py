b9ec97cdd08111a08aee9193416489264f3a4b83
Steve King, Jr <stevekx86@gmail.com>
2016-02-20 20:24:35 -0700

    Reimplemented SignUp form
    SignUp form now takes all the appropriate fields

--- src/forms.py
+++ src/forms.py
@@ -1,19 +1,24 @@
 from flask_wtf import Form
-from wtforms import StringField
-from wtforms import PasswordField
-from wtforms import BooleanField
+from wtforms import *
 from wtforms.validators import DataRequired
 
 
 class SignUpForm(Form):
-    username = StringField("User Name", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     confirm_password = PasswordField("Confirm Password", validators=[DataRequired()])
+    address_1 = StringField("Address Line 1", validators=[DataRequired()])
+    address_2 = StringField("Address Line 2")
+    city = StringField("City", validators=[DataRequired()])
+    state = StringField("State", validators=[DataRequired])
+    country = StringField("Country", validators=[DataRequired()])
+    phone_number = StringField("Phone Number", validators=[DataRequired])
+    profile_picture = FileField("Profile Picture")
 
 
 class LoginForm(Form):
-    username = StringField("User Name", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember Me")
 
